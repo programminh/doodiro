bed887f6386e694f9f4459f2b76fd6434ea29b99
Vincent Foley <vfoley@gmail.com>
2012-12-20 00:17:49 -0500

    Used bootstrap's icons

--- content/register_event.php
+++ content/register_event.php
@@ -3,10 +3,10 @@ function availability_class($reservations, $date, $hour) {
     if (array_key_exists($date, $reservations)) {
         $hours = $reservations[$date];
         if (array_key_exists($hour, $hours)) {
-            return $hours[$hour] ? "going" : "not-going";
+            return $hours[$hour] ? array("going", "icon-ok-sign") : array("not-going", "icon-remove-sign");
         }
     }
-    return "not-sure";
+    return array("not-sure", "");
 }
 
 $all_users = json_encode(User::find_all_names());
@@ -44,7 +44,12 @@ $c = count($event_dates);
                         <th><?php echo $event_dates[$i]["date"] ?></th>
                         <?php for ($h = 0; $h < 24; $h++): ?>
                             <?php if ($h >= $event_dates[$i]["start_hour"] && $h < $event_dates[$i]["end_hour"]): ?>
-                            <td class="changeable <?php echo availability_class($reservations, $event_dates[$i]["date"], $h) ?>">&nbsp;</td>
+                            <?php
+                            $classes = availability_class($reservations, $event_dates[$i]["date"], $h);
+                            $backgroundColor = $classes[0];
+                            $icon = $classes[1];
+                            ?>
+                            <td class="changeable <?php echo $backgroundColor ?>"><i class="<?php echo $icon ?>"></i></td>
                             <?php else: ?>
                             <td class="no-reservation">&nbsp;</td>
                             <?php endif ?>
@@ -60,12 +65,20 @@ $c = count($event_dates);
 <script>
 $(function () {
     var classes = ["going", "not-going", "not-sure"];
+    var icons = ["icon-ok-sign", "icon-remove-sign", ""];
     $(".changeable").click(function () {
         var that = $(this);
         var currentClass = that.prop("class").split(" ")[1]; // Yuck.
         var nextClass = classes[(classes.indexOf(currentClass) + 1) % 3];
         that.removeClass(currentClass);
         that.addClass(nextClass);
+
+        var icon = that.find("i");
+        var currentIcon = icon.prop("class");
+        var nextIcon = icons[(icons.indexOf(currentIcon) + 1) % 3];
+        icon.removeClass(currentIcon);
+        icon.addClass(nextIcon);
+
     });
 });
 </script>
